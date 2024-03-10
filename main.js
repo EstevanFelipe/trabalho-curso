1d2a203cb009e20a84f59da213d9eaf86ac6241d
EstevanFelipe <estevanfelipesilva@gmail.com>
2024-03-10 19:54:14 -0300

    update do código em 10-03-2024
    o código ainda não funciona, já refiz ele duas vezes, e ainda não houve uma resposta positiva do programa;

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById('meuForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var campoA = document.getElementById('campoA').value;
+    var campoB = document.getElementById('campoB').value;
+})
